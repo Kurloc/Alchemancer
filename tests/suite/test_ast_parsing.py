f82e7b1534c67c1a0f7ed51a624f3dd942e32f6a
Kurloc <Brandoningram1996@gmail.com>
2025-03-01 21:21:43 -0600

    * added basic support for unions and ctes in the APIs, needs more tests

--- tests/suite/test_ast_parsing.py
+++ tests/suite/test_ast_parsing.py
@@ -11,104 +11,145 @@ from tests.fixtures.models.user import User
 from tests.fixtures.test_dbs import psql_engine
 
 ReflectionHandler().init(
-    [(
-        "tests.fixtures.models",
-        Path.joinpath(
-            (
-                Path(__file__)
-                .parent  # suite dir
-                .parent  # tests dir
+    [
+        (
+            "tests.fixtures.models",
+            Path.joinpath(
+                (Path(__file__).parent.parent),  # suite dir  # tests dir
+                f"fixtures{os.sep}models",
             ),
-            f"fixtures{os.sep}models"
-        ),
-    )]
+        )
+    ]
 )
 
+
 def test_ast_functions_1():
-    token = QueryGenerator(psql_engine)._build_ast_tokens(
+    token = QueryGenerator(psql_engine)._convert_ast_to_sqlalchemy_column(
         """
 # amdql select / column string
 array_agg(aggregate_order_by(User.id, coalesce(User.account_balance, 0).desc()))
         """,
-        { # since this is a library for webservices, the implementor of amdql will be able to provide their
+        {  # since this is a library for webservices, the implementor of amdql will be able to provide their
             # functions they want to expose to users, there are also some defaults that are included.
-            'aggregate_order_by': aggregate_order_by,
-            'coalesce': coalesce,
-            'array_agg': array_agg,
-        }
+            "aggregate_order_by": aggregate_order_by,
+            "coalesce": coalesce,
+            "array_agg": array_agg,
+        },
     )
-    assert str(token) == "array_agg(user_account.id ORDER BY coalesce(user_account.account_balance, %(coalesce_1)s) DESC)"
+    assert (
+        str(token)
+        == "array_agg(user_account.id ORDER BY coalesce(user_account.account_balance, %(coalesce_1)s) DESC)"
+    )
+
 
 def test_ast_functions_2():
-    token = QueryGenerator(psql_engine)._build_ast_tokens(
+    token = QueryGenerator(psql_engine)._convert_ast_to_sqlalchemy_column(
         """
 # amdql select / column string
 array_agg(aggregate_order_by(coalesce(User.account_balance, 0), coalesce(User.account_balance, 0).desc()))
         """,
-        { # since this is a library for webservices, the implementor of amdql will be able to provide their
+        {  # since this is a library for webservices, the implementor of amdql will be able to provide their
             # functions they want to expose to users, there are also some defaults that are included.
-            'aggregate_order_by': aggregate_order_by,
-            'coalesce': coalesce,
-            'array_agg': array_agg,
-        }
+            "aggregate_order_by": aggregate_order_by,
+            "coalesce": coalesce,
+            "array_agg": array_agg,
+        },
     )
-    assert str(token) == "array_agg(coalesce(user_account.account_balance, %(coalesce_1)s) ORDER BY coalesce(user_account.account_balance, %(coalesce_2)s) DESC)"
+    assert (
+        str(token)
+        == "array_agg(coalesce(user_account.account_balance, %(coalesce_1)s) ORDER BY coalesce(user_account.account_balance, %(coalesce_2)s) DESC)"
+    )
+
 
 def test_ast_functions_3():
-    token = QueryGenerator(psql_engine)._build_ast_tokens(
+    token = QueryGenerator(psql_engine)._convert_ast_to_sqlalchemy_column(
         "coalesce(coalesce(array_agg(aggregate_order_by(User.id, User.id.desc()), '0'), 0))",
         {
-            'aggregate_order_by': aggregate_order_by,
-            'coalesce': coalesce,
-            'array_agg': array_agg,
-        }
+            "aggregate_order_by": aggregate_order_by,
+            "coalesce": coalesce,
+            "array_agg": array_agg,
+        },
     )
-    assert str(token) == "coalesce(coalesce(array_agg(user_account.id ORDER BY user_account.id DESC, :param_1), :coalesce_1))"
+    assert (
+        str(token)
+        == "coalesce(coalesce(array_agg(user_account.id ORDER BY user_account.id DESC, :param_1), :coalesce_1))"
+    )
+
 
 def test_ast_obj():
-    assert type(User) == type(QueryGenerator(psql_engine)._build_ast_tokens("User", {}))
-
-def test_ast_constant():
-    assert 1 == QueryGenerator(psql_engine)._build_ast_tokens("1", {})
-
-def test_ast_obj_field():
-    assert User.id == QueryGenerator(psql_engine)._build_ast_tokens("User.id", {})
-
-def test_ast_field_method_no_params():
-    assert str(User.id.desc()) == str(QueryGenerator(psql_engine)._build_ast_tokens("User.id.desc()", {}))
-
-def test_ast_function_no_params():
-    assert str(coalesce()) == str(QueryGenerator(psql_engine)._build_ast_tokens("coalesce()", {'coalesce': coalesce}))
-
-def test_ast_function_params():
-    assert str(coalesce(1, 0)) == str(QueryGenerator(psql_engine)._build_ast_tokens("coalesce(1, 0)", {'coalesce': coalesce}))
-
-def test_ast_function_with_function_params():
-    assert str(coalesce(None, func.now())) == str(QueryGenerator(psql_engine)._build_ast_tokens("coalesce(None, now())", {'coalesce': coalesce, 'now': func.now}))
-
-def test_ast_function_with_function_params_zoo():
-    assert (
-        str(coalesce(coalesce(None, func.now()), 0).desc())
-        == str(QueryGenerator(psql_engine)._build_ast_tokens(
-            "coalesce(coalesce(None, now()), 0).desc()",
-            {'coalesce': coalesce, 'now': func.now}
-        ))
+    assert type(User) == type(
+        QueryGenerator(psql_engine)._convert_ast_to_sqlalchemy_column("User", {})
     )
 
+
+def test_ast_constant():
+    assert 1 == QueryGenerator(psql_engine)._convert_ast_to_sqlalchemy_column("1", {})
+
+
+def test_ast_obj_field():
+    assert User.id == QueryGenerator(psql_engine)._convert_ast_to_sqlalchemy_column(
+        "User.id", {}
+    )
+
+
+def test_ast_field_method_no_params():
+    assert str(User.id.desc()) == str(
+        QueryGenerator(psql_engine)._convert_ast_to_sqlalchemy_column(
+            "User.id.desc()", {}
+        )
+    )
+
+
+def test_ast_function_no_params():
+    assert str(coalesce()) == str(
+        QueryGenerator(psql_engine)._convert_ast_to_sqlalchemy_column(
+            "coalesce()", {"coalesce": coalesce}
+        )
+    )
+
+
+def test_ast_function_params():
+    assert str(coalesce(1, 0)) == str(
+        QueryGenerator(psql_engine)._convert_ast_to_sqlalchemy_column(
+            "coalesce(1, 0)", {"coalesce": coalesce}
+        )
+    )
+
+
+def test_ast_function_with_function_params():
+    assert str(coalesce(None, func.now())) == str(
+        QueryGenerator(psql_engine)._convert_ast_to_sqlalchemy_column(
+            "coalesce(None, now())", {"coalesce": coalesce, "now": func.now}
+        )
+    )
+
+
+def test_ast_function_with_function_params_zoo():
+    assert str(coalesce(coalesce(None, func.now()), 0).desc()) == str(
+        QueryGenerator(psql_engine)._convert_ast_to_sqlalchemy_column(
+            "coalesce(coalesce(None, now()), 0).desc()",
+            {"coalesce": coalesce, "now": func.now},
+        )
+    )
+
+
 def test_ast_function_with_function_params_zoo_giga():
-    assert (
-        str(array_agg(
+    assert str(
+        array_agg(
             aggregate_order_by(
-                coalesce(
-                    User.account_balance, 0
-                ),
-                coalesce(
-                    User.account_balance, 0
-                ).desc(),
+                coalesce(User.account_balance, 0),
+                coalesce(User.account_balance, 0).desc(),
             )
-        ))
-        == str(QueryGenerator(psql_engine)._build_ast_tokens(
-        "array_agg(aggregate_order_by(coalesce(User.account_balance, 0), coalesce(User.account_balance, 0).desc()))"
-        , {'coalesce': coalesce, 'now': func.now, 'aggregate_order_by': aggregate_order_by, 'User': User, 'array_agg': array_agg}
-        ))
+        )
+    ) == str(
+        QueryGenerator(psql_engine)._convert_ast_to_sqlalchemy_column(
+            "array_agg(aggregate_order_by(coalesce(User.account_balance, 0), coalesce(User.account_balance, 0).desc()))",
+            {
+                "coalesce": coalesce,
+                "now": func.now,
+                "aggregate_order_by": aggregate_order_by,
+                "User": User,
+                "array_agg": array_agg,
+            },
+        )
     )
--- tests/suite/test_schema_generation.py
+++ tests/suite/test_schema_generation.py
@@ -9,6 +9,7 @@ from sqlalchemy_utils import database_exists, create_database
 from alchemancer.sqlalchemy.query_generator import QueryGenerator
 from alchemancer.sqlalchemy.reflection_handler import ReflectionHandler
 from tests.fixtures.models.base_model import BaseModel
+from tests.fixtures.models.role import Role
 from tests.fixtures.models.user import User
 from tests.fixtures.test_dbs import sqlite_engine, psql_engine
 
@@ -32,7 +33,7 @@ ReflectionHandler().init(
         (
             "tests.fixtures.models",
             Path.joinpath(
-                (Path(__file__).parent.parent),  # suite dir  # tests dir
+                Path(__file__).parent.parent,  # suite dir  # tests dir
                 f"fixtures{os.sep}models",
             ),
         )
@@ -94,7 +95,6 @@ def test_query(
             testing_connection.commit()
 
     query = QueryGenerator(engine).return_from_hql_query(test_dict)
-    print(query)
 
 
 def test_subquery():
@@ -102,16 +102,56 @@ def test_subquery():
         testing_connection.execute(
             insert(User).values(
                 [
-                    {"name": "JohnA", "account_balance": 100, "account_details": {"testing": {"inner": [1,2,3]}}},
-                    {"name": "JohnB", "account_balance": 101, "account_details": {"testing": {"inner": [1,2,3]}}},
-                    {"name": "JohnC", "account_balance": 102, "account_details":  {"testing": {"inner": [1,2,3]}}},
-                    {"name": "JohnD", "account_balance": 103, "account_details":  {"testing": {"inner": [1,2,3]}}},
-                    {"name": "JohnE", "account_balance": 104, "account_details":  {"testing": {"inner": [1,2,3]}}},
-                    {"name": "JohnAB", "account_balance": 200, "account_details":  {"testing": {"inner": [1,2,3]}}},
-                    {"name": "JohnBC", "account_balance": 201, "account_details":  {"testing": {"inner": [1,2,3]}}},
-                    {"name": "JohnCD", "account_balance": 202, "account_details":  {"testing": {"inner": [1,2,3]}}},
-                    {"name": "JohnDE", "account_balance": 203, "account_details":  {"testing": {"inner": [1,2,3]}}},
-                    {"name": "JohnEF", "account_balance": 204, "account_details":  {"testing": {"inner": [1,2,3]}}},
+                    {
+                        "name": "JohnA",
+                        "account_balance": 100,
+                        "account_details": {"testing": {"inner": [1, 2, 3]}},
+                    },
+                    {
+                        "name": "JohnB",
+                        "account_balance": 101,
+                        "account_details": {"testing": {"inner": [1, 2, 3]}},
+                    },
+                    {
+                        "name": "JohnC",
+                        "account_balance": 102,
+                        "account_details": {"testing": {"inner": [1, 2, 3]}},
+                    },
+                    {
+                        "name": "JohnD",
+                        "account_balance": 103,
+                        "account_details": {"testing": {"inner": [1, 2, 3]}},
+                    },
+                    {
+                        "name": "JohnE",
+                        "account_balance": 104,
+                        "account_details": {"testing": {"inner": [1, 2, 3]}},
+                    },
+                    {
+                        "name": "JohnAB",
+                        "account_balance": 200,
+                        "account_details": {"testing": {"inner": [1, 2, 3]}},
+                    },
+                    {
+                        "name": "JohnBC",
+                        "account_balance": 201,
+                        "account_details": {"testing": {"inner": [1, 2, 3]}},
+                    },
+                    {
+                        "name": "JohnCD",
+                        "account_balance": 202,
+                        "account_details": {"testing": {"inner": [1, 2, 3]}},
+                    },
+                    {
+                        "name": "JohnDE",
+                        "account_balance": 203,
+                        "account_details": {"testing": {"inner": [1, 2, 3]}},
+                    },
+                    {
+                        "name": "JohnEF",
+                        "account_balance": 204,
+                        "account_details": {"testing": {"inner": [1, 2, 3]}},
+                    },
                 ]
             )
         )
@@ -128,9 +168,110 @@ def test_subquery():
             .subquery()
         )
         users_over_100_with_a_in_name = select(
-            users_over_100.c.name, users_over_100.c.account_balance, users_over_100.c.testing
+            users_over_100.c.name,
+            users_over_100.c.account_balance,
+            users_over_100.c.testing,
         ).where(users_over_100.c.name.like("%A%"))
 
         print(users_over_100_with_a_in_name)
         results = testing_connection.execute(users_over_100_with_a_in_name)
         print(results.fetchall())
+
+
+def test_cte():
+    # query in sqlalchemy
+    cte = select(Role.id, Role.name, Role.parent_role_id)
+    union_cte = cte.union_all(
+        select(cte.c.id, cte.c.name, Role.id).join(
+            Role, Role.id == cte.c.parent_role_id
+        )
+    ).cte(recursive=True, name="roles_base_cte")
+
+    # query in json
+    x = {
+        "select": {"union_query": {"id": {}, "name": {}}},
+        "union_all": {
+            "name": "union_query",
+            "left": {
+                "select": {
+                    "Role": {
+                        "id": {},
+                        "name": {},
+                        "parent_role_id": {},
+                    }
+                },
+                "cte": {
+                    "recursive": True,
+                    "name": "cte_base",
+                },
+            },
+            "right": {
+                "select": {
+                    "cte_base": {
+                        "id": {},
+                        "name": {},
+                    }
+                },
+                "join": {
+                    "Role": {
+                        "select": {"id": {}},
+                        "where": {"Role.id__EQ": "cte_base.parent_role_id"},
+                    }
+                },
+            },
+        },
+    }
+    # query in sql
+    # WITH RECURSIVE roles_base_cte(id, name, parent_role_id) AS
+    #                    (SELECT role.id AS id, role.name AS name, role.parent_role_id AS parent_role_id
+    #                     FROM role
+    #                     UNION ALL
+    #                     SELECT
+    #                         anon_1.id AS id,
+    #                         anon_1.name AS name,
+    #                         role.id AS id_1
+    #                     FROM (
+    #                         SELECT role.id AS id, role.name AS name,
+    #                                role.parent_role_id AS parent_role_id
+    #                           FROM role
+    #                     ) AS anon_1
+    #                     JOIN role ON role.id = anon_1.parent_role_id)
+    # SELECT DISTINCT roles_base_cte.id, roles_base_cte.name, roles_base_cte.parent_role_id
+    # FROM roles_base_cte
+
+    # with psql_engine.connect() as testing_connection:
+    #     testing_connection.execute(
+    #         insert(User).values(
+    #             [
+    #                 {"name": "JohnA", "account_balance": 100, "account_details": {"testing": {"inner": [1,2,3]}}},
+    #                 {"name": "JohnB", "account_balance": 101, "account_details": {"testing": {"inner": [1,2,3]}}},
+    #                 {"name": "JohnC", "account_balance": 102, "account_details":  {"testing": {"inner": [1,2,3]}}},
+    #                 {"name": "JohnD", "account_balance": 103, "account_details":  {"testing": {"inner": [1,2,3]}}},
+    #                 {"name": "JohnE", "account_balance": 104, "account_details":  {"testing": {"inner": [1,2,3]}}},
+    #                 {"name": "JohnAB", "account_balance": 200, "account_details":  {"testing": {"inner": [1,2,3]}}},
+    #                 {"name": "JohnBC", "account_balance": 201, "account_details":  {"testing": {"inner": [1,2,3]}}},
+    #                 {"name": "JohnCD", "account_balance": 202, "account_details":  {"testing": {"inner": [1,2,3]}}},
+    #                 {"name": "JohnDE", "account_balance": 203, "account_details":  {"testing": {"inner": [1,2,3]}}},
+    #                 {"name": "JohnEF", "account_balance": 204, "account_details":  {"testing": {"inner": [1,2,3]}}},
+    #             ]
+    #         )
+    #     )
+    #     testing_connection.commit()
+    #
+    #     column_with_json = User.account_details
+    #     accessor_chain = ["testing", "inner"]
+    #     for item in accessor_chain:
+    #         column_with_json = column_with_json[item]
+    #
+    #     users_over_100 = (
+    #         select(User.name, User.account_balance, column_with_json.label("testing"))
+    #         .where(User.account_balance > 99, User.account_balance < 300)
+    #         .subquery()
+    #     )
+    #     users_over_100_with_a_in_name = select(
+    #         users_over_100.c.name, users_over_100.c.account_balance, users_over_100.c.testing
+    #     ).where(users_over_100.c.name.like("%A%"))
+    #
+    #     print(users_over_100_with_a_in_name)
+    #     results = testing_connection.execute(users_over_100_with_a_in_name)
+    #     print(results.fetchall())
